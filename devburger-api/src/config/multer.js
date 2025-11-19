cf00314116b9b8984c95f23d1065591475b5d778
arielvasconcelosgoncalves <ariel.goncalves@sou.unifeob.edu.br>
2025-11-19 14:45:03 -0300

    update: mais ajustes para o deploy

--- devburger-api/src/config/multer.js
+++ devburger-api/src/config/multer.js
@@ -1,12 +1,15 @@
 import multer from "multer";
-import { extname, resolve } from "node:path";
-import { v4 } from "uuid";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default {
   storage: multer.diskStorage({
     destination: resolve(__dirname, "..", "..", "uploads"),
-    filename: (request, file, callback) => {
-      return callback(null, v4() + extname(file.originalname));
+    filename: (req, file, cb) => {
+      return cb(null, file.originalname);
     },
   }),
 };
