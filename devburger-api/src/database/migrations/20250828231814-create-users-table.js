b9c329971baa6048917045a8f5b1736fc80c3aa4
Ariel Vasconcelos <ariel.goncalves@sou.unifeob.edu.br>
2025-11-19 17:22:11 -0300

    Fix comment syntax in migration file

--- devburger-api/src/database/migrations/20250828231814-create-users-table.js
+++ devburger-api/src/database/migrations/20250828231814-create-users-table.js
@@ -1,4 +1,4 @@
-@type {import('sequelize-cli').Migration}
+/** @type {import('sequelize-cli').Migration} */
 
 export async function up(queryInterface, Sequelize) {
   await queryInterface.createTable("users", {
