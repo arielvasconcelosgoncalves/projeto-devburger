74d5df0d037efdb96ad4d8d659fe3e006101033a
Ariel Vasconcelos <ariel.goncalves@sou.unifeob.edu.br>
2025-11-19 17:23:58 -0300

    Refactor migration to use ES module syntax

--- devburger-api/src/database/migrations/20250902231100-add-offer-column.js
+++ devburger-api/src/database/migrations/20250902231100-add-offer-column.js
@@ -1,16 +1,13 @@
-"use strict";
-
 /** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn("products", "offer", {
-      type: Sequelize.BOOLEAN,
-      defaultValue: false,
-      allowNull: false,
-    });
-  },
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn("products", "offer");
-  },
-};
+export async function up(queryInterface, Sequelize) {
+  await queryInterface.addColumn("products", "offer", {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+    allowNull: false,
+  });
+}
+
+export async function down(queryInterface, Sequelize) {
+  await queryInterface.removeColumn("products", "offer");
+}
