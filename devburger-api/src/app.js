2a2d37c2aefd896eb61ac7b090c75b5ae55f2e34
arielvasconcelosgoncalves <ariel.goncalves@sou.unifeob.edu.br>
2025-11-19 14:51:45 -0300

    update: ajustes para o deploy

--- devburger-api/src/app.js
+++ devburger-api/src/app.js
@@ -1,22 +1,29 @@
 import express from "express";
-import routes from "./routes.js";
-import "./database/index.js";
-import { resolve } from "node:path";
 import cors from "cors";
+import routes from "./routes.js";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+import "./database/index.js";
+
+// Corrigir __dirname no ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 class App {
   constructor() {
     this.app = express();
 
-    this.app.use(cors());
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
+    this.app.use(cors());
     this.app.use(express.json());
-    this.app.use("/product-file", express.static(resolve(__dirname, "..", "uploads")));
-    this.app.use("/category-file", express.static(resolve(__dirname, "..", "uploads")));
+
+    // CORREÇÃO AQUI — usar __dirname do ESM
+    this.app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
   }
 
   routes() {
