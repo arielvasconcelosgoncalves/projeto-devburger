87d2506ede3baeea176d840f47a912fc7509c9b3
arielvasconcelosgoncalves <ariel.goncalves@sou.unifeob.edu.br>
2025-11-21 08:04:43 -0300

    fix: atualizando projeto com boas práticas

--- devburger-interface/src/config/stripeConfig.js
+++ devburger-interface/src/config/stripeConfig.js
@@ -1,7 +1,5 @@
 import { loadStripe } from "@stripe/stripe-js";
 
-const stripePromise = loadStripe(
-  "pk_test_51S6h9zIZxvFePJ3icTvxCEyUFdpxZ7EGbJHTtsFP7JCyln6bpOKmyWmRLOpZVXiia7ESf5K11UTCAgvQcj1OYWF700FyPWkhXw"
-);
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
 
 export default stripePromise;
